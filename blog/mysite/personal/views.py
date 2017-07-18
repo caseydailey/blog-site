c0a4f886ff628ce61096ecfcde466f141795a7f1
Casey Dailey <casey@feasttogether.org>
2017-07-18 10:49:46 -0500

    boom

---
+++ blog/mysite/personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content': ['if you would like to contact me, please email', 'casey@feasttogether.org']})
+
+
